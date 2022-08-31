f8aa9cba12f1e721792f5e9b182ce2580ec41f91
kader326 <kcompaore73@gmail.com>
2022-08-31 15:59:16 +0100

    commit

---
+++ week2/day1/exercises xp/exercice 3.js
@@ -0,0 +1,5 @@
+const celsius = prompt("Enter a celsius value: ");
+
+const fahrenheit = ((celsius /5)*9) + 32
+
+console.log(`${celsius} degree celsius vaut ${fahrenheit} degree fahrenheit.`);
---
+++ week2/day1/exercises xp/exercice xp.js
@@ -0,0 +1,4 @@
+let a = "I eat beans ";
+    let b = " every dinner";
+    let c = a+b;
+   console.log (c);
